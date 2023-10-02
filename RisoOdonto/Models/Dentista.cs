2958d7a108d1e8303e71b1a2bfa17f4489ef901f
Kelvin Bryan <k.bryan924@gmail.com>
2023-10-02 20:04:55 -0300

    criação do model do Dentista.

---
+++ RisoOdonto/Models/Dentista.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RisoOdonto.Models
+{
+    [Table("Dentista")]
+    public class Dentista
+    {
+
+        [Key]
+        public int IdDentista { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Nome { get; set; }
+        [Required]
+        [MaxLength(150)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Login_Dentista { get; set; }
+        [Required]
+        [MaxLength(6)]
+        public string Senha { get; set; }
+        [Required]
+        public int Especialidade { get; set; }
+    }
+}
