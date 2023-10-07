81b8dee217c38495225216f590279fd6b0f7809f
Kelvin Bryan <k.bryan924@gmail.com>
2023-10-06 21:44:16 -0300

    alterandoa para ui

---
+++ RisoOdontoUI/utilitários/Limpar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+
+namespace RisoOdontoDSKTP.utilitários
+{
+    public static class Limpar
+    {
+        public static void ClearControl(Control ctrl)
+        {
+            foreach (Control c in ctrl.Controls)
+            {
+                if (c is TextBox)
+                {
+                    ((TextBox)c).Text = string.Empty;
+                }
+                else if (c is Label)
+                {
+                    ((Label)c).Text = string.Empty;
+                }
+                else if (c is DropDownList)
+                {
+                    ((DropDownList)c).ClearSelection();
+                }
+                else if (c is RadioButtonList)
+                {
+                    ((RadioButtonList)c).ClearSelection();
+                }
+                ClearControl(c);
+
+
+
+            }
+        }
+    }
+}
