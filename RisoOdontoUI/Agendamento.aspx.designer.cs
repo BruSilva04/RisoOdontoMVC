e87b6deb7765fac3de30637c32dd6c7d6626804d
Kelvin Bryan <k.bryan924@gmail.com>
2023-10-16 22:06:43 -0300

    agendamento finalizando

--- RisoOdontoUI/Agendamento.aspx.designer.cs
+++ RisoOdontoUI/Agendamento.aspx.designer.cs
@@ -22,23 +22,5 @@ namespace RisoOdontoUI
         /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// Controle Calendario.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Calendar Calendario;
-
-        /// <summary>
-        /// Controle lblMsg.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblMsg;
     }
 }
