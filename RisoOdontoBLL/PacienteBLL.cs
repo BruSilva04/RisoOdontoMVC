069bc53a07f3fe080634b15c1725ec4c18b5570c
brunoS <silvabrunin503@gmail.com>
2023-10-19 21:47:20 -0300

    Cadastrar ok

---
+++ RisoOdontoBLL/PacienteBLL.cs
@@ -0,0 +1,54 @@
+﻿using RisoOdontoDAL;
+using RisoOdontoDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RisoOdontoBLL
+{
+    public class PacienteBLL
+    {
+        //variavel
+        PacienteDAL objBLL = new PacienteDAL();
+
+        //autenticar
+        public PacienteDTO AutenticarPaciente(string Email, string Senha)
+        {
+            PacienteDTO paciente = objBLL.AutenticarCliente(Email, Senha);
+            return paciente;
+        }
+
+        //CRUD
+        //Create
+        public void Cadastrar(PacienteDTO objCad)
+        {
+            objBLL.CadastrarCliente(objCad);
+        }
+
+        //Read
+        public List<PacienteDTO> Listar()
+        {
+            return objBLL.ListarPaciente();
+        }
+
+        //Delete
+        public void Excluir(int objDel)
+        {
+            objBLL.ExcluirPaciente(objDel);
+        }
+
+        //Update
+        public void Editar(PacienteDTO objEdita)
+        {
+            objBLL.EditarPaciente(objEdita);
+        }
+
+        //CarregaDDL
+        public List<TipoUsuarioDTO> CarregaTpUsuarioDDL()
+        {
+            return objBLL.CarregaDDL();
+        }
+    }
+}
---
+++ RisoOdontoDTO/PacienteDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RisoOdontoDTO
+{
+    public class PacienteDTO
+    {
+        public int IdPaciente { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Telefone { get; set; }
+        public DateTime DataNasciemento { get; set; }
+        public int CPF { get; set; }
+        public string Cidade { get; set; }
+        public string Endereço { get; set; }
+        public string Senha { get; set; }
+        public string TpUsuario { get; set; }
+    }
+}
